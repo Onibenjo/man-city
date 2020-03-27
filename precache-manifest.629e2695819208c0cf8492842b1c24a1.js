e8b25d0c9ae62bf6b10667fa21bc59408d43bf11
dammyayans <dammyayans560@gmail.com>
2020-03-27 01:27:13 -0700

    Updates

---
+++ precache-manifest.629e2695819208c0cf8492842b1c24a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294b34e3f5a648e503eb6223d7209abd",
+    "url": "/man-city/index.html"
+  },
+  {
+    "revision": "a5347b814f4bcbf21f5a",
+    "url": "/man-city/static/css/main.7da2ec40.chunk.css"
+  },
+  {
+    "revision": "18a205d287f608b14a67",
+    "url": "/man-city/static/js/2.12278c6c.chunk.js"
+  },
+  {
+    "revision": "48a7f982264b141da44db572a26ecb94",
+    "url": "/man-city/static/js/2.12278c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5347b814f4bcbf21f5a",
+    "url": "/man-city/static/js/main.a74dfd76.chunk.js"
+  },
+  {
+    "revision": "16a95f677d7558477c17",
+    "url": "/man-city/static/js/runtime-main.83c48947.js"
+  },
+  {
+    "revision": "5432bcd5600bfba5ec63967d7efb05b9",
+    "url": "/man-city/static/media/Otamendi.5432bcd5.png"
+  },
+  {
+    "revision": "6de3706ade22bf1989b3853dda736986",
+    "url": "/man-city/static/media/featured_player.6de3706a.png"
+  },
+  {
+    "revision": "dd26db2771a362bbd922fa4686b5218a",
+    "url": "/man-city/static/media/jersey.dd26db27.jpg"
+  },
+  {
+    "revision": "1d5b64ec5f8fe8089fd9370edd244ac4",
+    "url": "/man-city/static/media/manchester_city_logo.1d5b64ec.png"
+  }
+]);
